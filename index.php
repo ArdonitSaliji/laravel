6210fec14e53fb928c92d7bfa5eb9d1cc97873aa
ArdonitSaliji <ardonit.1980@gmail.com>
2023-03-15 16:00:37 +0100

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
+// $users = [
+//     [
+//         'name' => 'test test',
+//         'email' => 'test@gmail.com',
+//         'password' => '12341234'
+//     ],
+    
+//     [
+//         'name' => 'john doe',
+//         'email' => 'johndoe@gmail.com',
+//         'password' => '12341234'
+//     ],
+    
+//     [
+//         'name' => 'jane doe',
+//         'email' => 'janedoe@gmail.com',
+//         'password' => '12341234'
+//     ]
+    
+// ];
 
+// file_put_contents('users2.json', json_encode($users))
 
 ?>
