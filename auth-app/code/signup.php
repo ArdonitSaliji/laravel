403dcdcf58367ab95101275c64dada97b67acac7
ArdonitSaliji <ardonit.1980@gmail.com>
2023-03-17 21:51:53 +0100

    login code

--- auth-app/code/signup.php
+++ auth-app/code/signup.php
@@ -1,16 +1,20 @@
-<!-- $usersFile = file_get_contents('users.json');
-    $users = json_decode($usersFile, true);
+<?php
+
+$usersFile = file_get_contents('users.json');
+$users = json_decode($usersFile, true);
+
+for($i = 0; $i < count($users); $i++) { 
     
-    for($i = 0; $i < count($users); $i++) {
-        if($users[$i]['email'] === $req->email) {
-            return response()->json(['message' => 'Account already exists!', 'status' => 405], 405);
-        } 
+    if($users[$i]['email']===$req->email) {
+        return response()->json(['message' => 'Account already exists!', 'status' => 405], 405);
     }
-    
-    
-    $newUser = ['name' => $req->name, 'email' => $req->email, 'password' => $req->password];
+}
+
+    $newUser = ['name' => $req->name, 'email' => $req->email, 'password' =>
+    $req->password];
     array_push($users, $newUser);
-    
+
     file_put_contents('users.json', json_encode($users));
-    
-    return response()->json(['users' => $users, 'message' => 'Account created successfully!', 'status' => 200], 200); -->
+
+    return response()->json(['users' => $users, 'message' => 'Account created
+    successfully!', 'status' => 200], 200);
--- auth-app/routes/api.php
+++ auth-app/routes/api.php
@@ -7,34 +7,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-$users = [
-    [
-        'name' => 'test test',
-        'email' => 'test@gmail.com',
-        'password' => '12341234'
-    ],
-    
-    [
-        'name' => 'john doe',
-        'email' => 'johndoe@gmail.com',
-        'password' => '12341234'
-    ],
-    
-    [
-        'name' => 'jane doe',
-        'email' => 'janedoe@gmail.com',
-        'password' => '12341234'
-    ]
-    
-];
+
     
 // file_put_contents('users2.json', json_encode($users));
 
 // Handle Login
 
 Route::post('/login', function (Request $request) {
+    $email = $request->email;
+    $password = $request->password;
 
+    $json = file_get_contents('users.json');
+    $users = json_decode($json, true);
     
+    for ($i = 0; $i < count($users); $i++) {
+
+        $getEmail = $users[$i]['email'];
+        $getPassword = $users[$i]['password'];
+        
+        if($getEmail == $email && $getPassword == $password) {
+            return response()->json(['message' => 'success', 'status' => 200], 200);
+        }            
+    }
+
+    return response()->json(['message' => 'Incorrect Credentials!', 'status' => 403], 403);
 });
 
 
